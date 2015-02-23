7d6063d9d0b5437e603e91b82b4b7b10188fc8bf
Tomo Hung <tomohung@gmail.com>
2015-02-23 19:42:16 +0800

    get_symbol helper to clean code

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -4,6 +4,7 @@ require 'oo_tic_tac_toe.rb'
 class GameController < ApplicationController
   
   before_action :create_game
+  before_action :get_board_data, only: [:show, :create]
 
   def index
 
@@ -14,7 +15,7 @@ class GameController < ApplicationController
   end
 
   def create
-    
+    @board
     redirect_to game_show_path
   end
 
@@ -27,6 +28,16 @@ private
     robot = Robot_Player.new("Robot")
     session[:board] = TicTacToeBoard.new(player, robot)
 
+  end
+
+  def get_board_data
+    
+    @board = session[:board]
+    player = session[:board]["user1"]
+    host = session[:board]["user2"]
+    
+    @player_picked_numbers = player["picked_numbers"]
+    @host_picked_numbers = host["picked_numbers"]
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def get_symbol(number)
+    if @player_picked_numbers.include? number
+      'O'
+    elsif @host_picked_numbers.include? number
+      'X'
+    else
+      link_to number.to_s, game_create_path(id: number.to_s), method: :post
+    end
+  end
+
 end
