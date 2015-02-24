d24685f5683dfea5a8db0fb2545156d3bc067132
Tomo Hung <tomohung@gmail.com>
2015-02-24 07:02:09 +0000

    backup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,9 @@ module ApplicationHelper
     elsif @host_picked_numbers.include? number
       'X'
     else
-      link_to number.to_s, game_create_path(id: number.to_s), method: :post
+      link_to game_create_path(id: number.to_s), method: :post do
+        number.to_s
+      end
     end
   end
 
