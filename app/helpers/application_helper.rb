dc5d69c2a13d2d111da469d8456f12b891a9f276
Tomo Hung <tomohung@gmail.com>
2015-02-24 21:27:58 +0800

    modified UI & css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,3 @@
 module ApplicationHelper
 
-  def get_symbol(number)
-    if @player_picked_numbers.include? number
-      'O'
-    elsif @host_picked_numbers.include? number
-      'X'
-    else
-      link_to game_create_path(id: number.to_s), method: :post do
-        number.to_s
-      end
-    end
-  end
-
 end
